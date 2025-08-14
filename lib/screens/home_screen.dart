3d7f9775c49fa3df7d81aab283c0a39cdd5ee572
Abu Bakkar Siddique Sakib <abubakkarsakib360@gmail.com>
2025-08-15 03:56:18 +0600

    Todo Ui Desiging and Logic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,10 +8,100 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Todo> todoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        elevation: 10,
+        title: Text(
+          'Todo List',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      // drawer: Drawer(),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          Todo todo = todoList[index];
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            shadowColor: Colors.grey,
+            child: ListTile(
+              onTap: () {},
+              contentPadding: EdgeInsets.symmetric(horizontal: 20),
+              leading: Icon(
+                Icons.check_box_outline_blank_outlined,
+                color: Colors.black,
+              ),
+              title: Text(todo.title),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(todo.description),
+                  Text('Create Date: ${todo.dateCreated}'),
+                ],
+              ),
+              trailing: Text(
+                'Pending',
+                style: TextStyle(color: Colors.orange, fontSize: 14),
+              ),
+            ),
+          );
+        },
+      ),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange.withOpacity(0.7),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                width: double.infinity,
+                height: 200,
+                child: Column(children: [
+                  SizedBox(height: 30,),
+                  ListTile(
+                    leading: Icon(Icons.add_task_outlined),
+                    title: Text('Add New Task'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.incomplete_circle_rounded),
+                    title: Text('Mark as Complete'),
+                    onTap: () {},
+                  ),
 
+                ]),
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
     );
   }
+}
+
+class Todo{
+  final String title;
+  final String description;
+  final DateTime dateCreated;
+  bool isComplete;
+  Todo({
+    required this.title,
+    required this.description,
+    required this.isComplete,
+    required this.dateCreated,
+  });
 }
