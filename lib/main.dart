a36a71bed2c35e883659dcdedefe93194651e164
Abu Bakkar Siddique Sakib <abubakkarsakib360@gmail.com>
2025-08-14 22:40:30 +0600

    Todo App Practice

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home_screen.dart';
+
+void main(){
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
