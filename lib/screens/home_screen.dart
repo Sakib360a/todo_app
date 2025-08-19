e0197ea89a30134a74e4585c2ea2bf87bd56a858
Abu Bakkar Siddique Sakib <abubakkarsakib360@gmail.com>
2025-08-19 14:08:12 +0600

    Glassmorphism Effect tried for Drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:todo_app/screens/add_new_todo_screen.dart';
 
 import '../classes/todo.dart';
@@ -18,6 +19,14 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Image.asset('assets/icons/BurgerMenu.png',scale: 1,), // your custom icon
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          ),
+        ),
+
         title: Text(
           'Todo List',
           style: TextStyle(
@@ -32,8 +41,8 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset('assets/icons/noTasks.png',scale: 6,),
-                  SizedBox(height: 5,),
+                  Image.asset('assets/icons/noTasks.png', scale: 6),
+                  SizedBox(height: 5),
                   Text(
                     'No tasks',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -64,7 +73,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         },
                         contentPadding: EdgeInsets.symmetric(horizontal: 20),
                         leading: Icon(
-                          Icons.check_box_outline_blank_outlined,
+                          Icons.circle_outlined,
                           color: Colors.black,
                         ),
                         title: Text(todo.title),
@@ -86,14 +95,77 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
       drawer: Drawer(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                  
-              ],
-            )
-          ],
+        backgroundColor: Colors.black.withOpacity(.3),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Column(
+            children: [
+                Container(
+                  alignment: Alignment.center,
+                 // clipBehavior: Clip.none,
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.orange.withOpacity(1),
+                    //color: Colors.white.withOpacity(0.15), // frosted layer
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(20),bottomRight: Radius.circular(20)),
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.3),
+                      width: 1.5,
+                  ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 20,
+                        offset: Offset(0, 8),
+                      ),
+                      BoxShadow( // inner glow
+                        color: Colors.white.withOpacity(0.2),
+                        blurRadius: 10,
+                        spreadRadius: -5,
+                        offset: Offset(-5, -5),
+                      ),
+                    ],
+                  ),
+                  child: Stack(children: [
+                    Center(
+                      child: CircleAvatar(radius: 40,
+                              backgroundImage: AssetImage('assets/images/SakibAvatar.jpeg'),
+                  ),
+
+                  ),
+                    Positioned(
+                      top: 140,
+                      left: 70,right: 55,
+                      child: Text("Abu Bakkar Sakib",style: TextStyle(color: Colors.white,
+                        fontSize: 22,fontWeight: FontWeight.w600,wordSpacing: -2,
+                      ),),
+                    ),
+                  ]
+                  ),
+
+                ),
+                SizedBox(height: 10,),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.white.withOpacity(0.05),
+                  child: ListTile(
+                    onTap: (){},
+                    title: Text('All Tasks',style: TextStyle(color: Colors.white),),
+                   // subtitle: Text('Test'),
+                  ),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.white.withOpacity(0.05),
+                  child: ListTile(
+                    onTap: (){},
+                    title: Text('Pending Tasks',style: TextStyle(color: Colors.white),),
+                   // subtitle: Text('Test'),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -108,7 +180,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: 120,
                 child: Column(
                   children: [
-                    SizedBox(height: 30,),
+                    SizedBox(height: 30),
                     Container(
                       margin: EdgeInsets.symmetric(horizontal: 16),
                       padding: EdgeInsets.symmetric(horizontal: 16),
@@ -120,8 +192,17 @@ class _HomeScreenState extends State<HomeScreen> {
                         borderRadius: BorderRadius.circular(16),
                       ),
                       child: ListTile(
-                        leading: Image.asset('assets/icons/addNewTask.png',scale: 16,),
-                        title: Text('Add New Task',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500),),
+                        leading: Image.asset(
+                          'assets/icons/addNewTask.png',
+                          scale: 16,
+                        ),
+                        title: Text(
+                          'Add New Task',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                         onTap: () async {
                           Navigator.pop(context);
                           Todo? todo = await Navigator.push(
